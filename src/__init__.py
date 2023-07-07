6ce79d92ab2412ec22997b3ac1513f17a2813d96
Farzad Salajegheh <salajegheh@outlook.com>
2023-07-07 14:47:55 -0400

    docstring updated

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,10 @@
+"""
+This module initializes the application and provides functions for suggestion and file reading.
+
+Functions:
+- suggest_answer: Generates a suggested answer based on given inputs.
+- read_string_from_file: Reads a string from a file.
+"""
+
 from .suggestion import suggest_answer
 from .utils import read_string_from_file
